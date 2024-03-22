7e2f48ab3cc579e0f729e5eb63276f8be1030a5f
niickyds <ntsd1158@gmail.com>
2024-03-22 14:31:42 -0500

    basic fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = require("../utils/config");
+const { JWT_SECRET } = require("../utils/config");
 const User = require("../models/user");
 const {
   BadRequestError,
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const { JWT_SECRET = "not secret" } = process.env;
 
 module.exports = { JWT_SECRET };
