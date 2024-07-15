de79672272d536137f67e3bbc9942f65e00de39b
adeptexotica <ntsd1158@gmail.com>
2024-07-14 20:01:37 -0500

    route fix

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { PORT = 3001 } = process.env;
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const { errorHandler } = require("./middlewares/errorHandler.js");
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger.js");
+const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(console.error);
 
 app.use(express.json());
 app.use(cors());
@@ -16,13 +23,6 @@ app.use("/", mainRouter);
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`server working ${PORT}`);
