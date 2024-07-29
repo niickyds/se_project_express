8779d162f238cc63192ca0e89aefa92327567815
adeptexotica <ntsd1158@gmail.com>
2024-07-29 14:44:16 -0500

    test: route testing

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,19 @@ const { login, createUser } = require("../controllers/users");
 router.post("/signin", login);
 router.post("/signup", createUser);
 router.use("/users", userRouter);
-router.use("/items", clothingItem);
+router.use(
+  "/items",
+  (req, res, next) => {
+    console.log("Items route hit");
+    next();
+  },
+  clothingItem,
+);
+
+router.use((req, res) => {
+  console.log("Default route hit");
+  res.status(404).json({ message: "Router not found" });
+});
 
 router.use((req, res) => {
   res.status(NotFoundError).send({ message: "Router not found" });
