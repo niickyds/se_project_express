ff723093b8ecbf58a0d7b422f62b7d12f4bb707e
adeptexotica <ntsd1158@gmail.com>
2024-08-02 13:26:43 -0500

    test fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const clothingItem = require("./clothingItem");
 const userRouter = require("./users");
-const { NotFoundError } = require("../utils/errors");
+const NotFoundError = require("../errors/NotFoundError");
 const { login, createUser } = require("../controllers/users");
+const signRouter = require("./sign");
 
 router.post("/signin", login);
 router.post("/signup", createUser);
 router.use("/users", userRouter);
 router.use("/items", clothingItem);
+
+router.use("/", signRouter);
 
 // router.use((req, res) => {
 //   console.log("Default route hit");
