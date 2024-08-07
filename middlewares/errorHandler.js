4ba38ba0856e042b3ac1e73896853e439fd28e5e
adeptexotica <ntsd1158@gmail.com>
2024-08-06 19:20:54 -0500

    test

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,11 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
+  console.log(res);
+  console.log(req);
 
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
-    message: statusCode === 500 ? "An error occurred on the " : message,
+    message: statusCode === 500 ? "An error occurred on the server" : message,
   });
 };
 
