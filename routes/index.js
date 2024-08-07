4891102ffdc7122ada7889a7ef8096c2b9ce81b6
adeptexotica <ntsd1158@gmail.com>
2024-08-06 19:42:36 -0500

    index "/" test

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@ const userRouter = require("./users");
 const NotFoundError = require("../errors/NotFoundError");
 const { login, createUser } = require("../controllers/users");
 const { auth } = require("../middlewares/auth");
-const {
-  validateReturningUser,
-  validateNewUser,
-} = require("../middlewares/validation");
+// const {
+//   validateReturningUser,
+//   validateNewUser,
+// } = require("../middlewares/validation");
 
-router.post("/signin", validateReturningUser, login);
-router.post("/signup", validateNewUser, createUser);
+router.post("/signin", login);
+router.post("/signup", createUser);
 router.use("/users", userRouter);
 router.use("/items", clothingItem);
+router.use("/");
 
 router.use((req, res, next) => {
   console.log(req);
